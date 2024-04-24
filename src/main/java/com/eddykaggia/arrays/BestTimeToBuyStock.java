bffd899017550b43ab7124b7ea76f8dac72b2685
EddyKaggia <kagzcodes@gmail.com>
2024-04-24 14:51:51 -0700

    feat: optimal solution to Best time to buy stock

---
+++ src/main/java/com/eddykaggia/arrays/BestTimeToBuyStock.java
@@ -0,0 +1,28 @@
+package com.eddykaggia.arrays;
+
+public class BestTimeToBuyStock {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int result = maxProfit(prices);
+
+        System.out.println(result);
+    }
+
+    public static int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+
+        return maxProfit;
+    }
+}
