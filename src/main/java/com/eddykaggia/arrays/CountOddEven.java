c879b4cbe5d8516054b2753748d31417d0f2e1f6
EddyKaggia <kagzcodes@gmail.com>
2025-04-18 06:50:28 -0700

    feat: count odd and even nums in array

---
+++ src/main/java/com/eddykaggia/arrays/CountOddEven.java
@@ -0,0 +1,24 @@
+package com.eddykaggia.arrays;
+
+import java.util.Arrays;
+
+public class CountOddEven {
+    public static int[] countNum(int[] inputNum) {
+        int[] output = new int[2];
+
+        for (int i = 0; i < inputNum.length; i++) {
+            if (inputNum[i]%2 == 0) {
+                output[0]++;
+            } else {
+                output[1]++;
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countNum(new int[]{1, 2, 3, 4, 5, 6}))); // [3, 3]
+        System.out.println(Arrays.toString(countNum(new int[]{1,2,3,4,5}))); // [1, 2]
+    }
+  
+}
