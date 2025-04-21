8203a1724c45b6c363bad7311b287001099c2b11
EddyKaggia <kagzcodes@gmail.com>
2025-04-21 06:28:28 -0700

    feat: remove duplicates array using ArrayList

--- src/main/java/strings/RemoveDuplicates.java
+++ src/main/java/strings/RemoveDuplicates.java
@@ -1,5 +1,40 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RemoveDuplicates {
-    
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> inputList) {
+        ArrayList<Integer> output = new ArrayList<>();
+
+        for (int i = 0; i < inputList.size(); i++) {
+            int current = inputList.get(i);
+            boolean isDuplicate = false;
+
+            for (int j = 0; j <output.size(); j++) {
+                if (output.get(j) == current) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if(!isDuplicate) {
+                output.add(current);
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>() {{
+            add(1);
+            add(3);
+            add(2);
+            add(3);
+            add(4);
+            add(1);
+        }};
+        
+        System.out.println(removeDuplicates(list));
+    }
 }
