9556bab371e3fb54a9f9dba68cb22244dbdf35ab
EddyKaggia <kagzcodes@gmail.com>
2025-04-24 16:18:58 -0700

    feat:Fibonacci refactored

--- src/main/java/numbers/Fibonacci.java
+++ src/main/java/numbers/Fibonacci.java
@@ -1,18 +1,31 @@
 package numbers;
 
+import java.util.Arrays;
+
 public class Fibonacci {
-    public static void main (String [] args) {
-        int n = 10; // you can change this to any number of terms
-        int first = 0;
-        int second = 1;
+    public static int fib(int input) {
+        if (input == 0) return 0;
+        if (input == 1) return 1;
 
-        // print the first n Fibonacci numbers
+        return fib(input - 2) + fib(input -1);
+    }
+
+    public static int[] fibonacci(int n) {
+        int[] fibArray = new int[n];
+
         for (int i = 0; i < n; i++) {
-            System.out.print(first + " ");
-
-            int next = first + second;
-            first = second;
-            second = next;
+            fibArray[i] = fib(i);
+            System.out.println("Current fibonacci number is: " + fib(i));
         }
+
+        System.out.println(Arrays.toString(fibArray));
+
+        return fibArray;
+    }
+
+    public static void main (String [] args) {
+        System.out.println(fibonacci(10));
+        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(19));
     }
 }
