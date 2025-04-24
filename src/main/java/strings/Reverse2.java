66738a1b20609475bfd62510f95ec81df1e95f0d
EddyKaggia <kagzcodes@gmail.com>
2025-04-23 20:38:29 -0700

    feat:Reverse 2 refactored

--- src/main/java/strings/Reverse2.java
+++ src/main/java/strings/Reverse2.java
@@ -1,5 +1,19 @@
 package strings;
 
 public class Reverse2 {
-    
+    public static String reverse(String inputStr) {
+        String outputStr = "";
+
+        for (int i = inputStr.length() - 1; i >= 0; i--) {
+            outputStr += inputStr.charAt(i);
+        }
+
+        return outputStr;
+    }
+
+    public static void main(String [] args) {
+        System.out.println(reverse("rack"));
+        System.out.println(reverse("eulb"));
+        System.out.println(reverse("rehcir"));
+    }
 }
