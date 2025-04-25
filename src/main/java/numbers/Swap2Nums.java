6ed24be461d1e8d0a39a53c27cf135eb5c4dfb92
EddyKaggia <kagzcodes@gmail.com>
2025-04-24 17:41:51 -0700

    feat:Swap 2 numbers

--- src/main/java/numbers/Swap2Nums.java
+++ src/main/java/numbers/Swap2Nums.java
@@ -1,5 +1,24 @@
 package numbers;
 
+import java.util.Arrays;
+
 public class Swap2Nums {
-    
+    public static int[] swap(int num1, int num2) {
+        int[] output = new int[2];
+
+        num1 += num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        output[0] = num1;
+        output[1] = num2;
+
+        return output;
+    }
+
+    public static void main(String [] args) {
+        System.out.println(Arrays.toString(swap(0, 1)));
+        System.out.println(Arrays.toString(swap(4, 2)));
+        System.out.println(Arrays.toString(swap(9, 3)));
+    }
 }
