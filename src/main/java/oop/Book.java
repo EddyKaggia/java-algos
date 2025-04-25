117f258c221047babff35d8c1525643950262d38
EddyKaggia <kagzcodes@gmail.com>
2025-04-25 16:42:41 -0700

    feat:Book class refactored

--- src/main/java/oop/Book.java
+++ src/main/java/oop/Book.java
@@ -1,5 +1,27 @@
 package oop;
 
+class BookData {
+    String title;
+    String author;
+
+    BookData(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String toString() {
+        return "Title: " + title + "| Author: " + author;
+    }
+}
+
 public class Book {
-    
+    public static BookData create(String title, String author) {
+        return new BookData(title, author);
+    }
+
+    public static void main(String [] args) {
+        System.out.println(create("The Fifth Season", "N. K. Jemisin"));
+        System.out.println(create("The Sorcere of The Wildeeps", "K. Ashante Williams"));
+        System.out.println(create("A Master of Djinn", "P. Djelli Clark"));
+    }
 }
