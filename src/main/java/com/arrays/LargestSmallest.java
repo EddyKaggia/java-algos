75569f0670a3b032b022a58af8234665f5cb994c
EddyKaggia <kagzcodes@gmail.com>
2025-04-25 15:42:58 -0700

    feat:Largest Smallest array created

--- src/main/java/com/arrays/LargestSmallest.java
+++ src/main/java/com/arrays/LargestSmallest.java
@@ -1,5 +1,22 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 public class LargestSmallest {
-    
+    public static int[] largestSmallest(int[] num) {
+       int[] output = {num[0], num[1]};
+
+       for (int i : num) {
+        if (i > output[0]) output[0] = i;
+        if (i < output[1]) output[0] = i;
+       }
+
+       return output;
+    }
+
+    public static void main(String [] args) {
+        System.out.println(Arrays.toString(largestSmallest(new int[]{2,4,1,9,200})));
+        System.out.println(Arrays.toString(largestSmallest(new int[]{81,24,11,109,210})));
+        System.out.println(Arrays.toString(largestSmallest(new int[]{21,4,199,91,1200})));
+    }
 }
