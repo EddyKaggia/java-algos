38a2db3fce257e126633f7279948c52e8bbae08e
EddyKaggia <kagzcodes@gmail.com>
2025-04-30 06:08:20 -0700

    feat: merge refactored

--- src/main/java/arrays/Merge.java
+++ src/main/java/arrays/Merge.java
@@ -3,20 +3,20 @@ package arrays;
 import java.util.Arrays;
 
 public class Merge {
-    public static int[] merge(int[] arr1, int[] arr2) {
-        int[] merged = new int[arr1.length + arr2.length];
+   public static int[] merge(int[] arr1, int[] arr2) {
+    int[] merged = new int[arr1.length + arr2.length];
 
-        int i = 0, j = 0, k = 0;
+    int i = 0, j = 0, k = 0;
 
-        while (i < arr1.length && j < arr2.length) {
-            merged[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
-        }
-
-        while (i < arr1.length) merged[k++] = arr1[i++];
-        while (j < arr2.length) merged[k++] = arr2[j++];
-
-        return merged;
+    while (i < arr1.length && j < arr2.length) {
+        merged[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
     }
+
+    while (i < arr1.length) merged[k++] = arr1[i++];
+    while (j < arr2.length) merged[k++] = arr2[j++];
+
+    return merged;
+   }
 
     public static void main (String [] args) {
         System.out.println(Arrays.toString(merge(new int[]{1, 3, 5, 23, 9}, new int[]{2, 4, 6, 11, 15, 17})));
