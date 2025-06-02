622e79c6fc01653c6c50d0c5c4b22bb91264e590
EddyKaggia <kagzcodes@gmail.com>
2025-06-01 19:34:55 -0700

    feat: has loop created and solved

---
+++ src/main/java/leetcode/linkedlists/HasLoop.java
@@ -0,0 +1,19 @@
+package leetcode.linkedlists;
+
+public class HasLoop {
+
+    public boolean hasLoop(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
