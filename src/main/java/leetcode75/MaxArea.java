f79c26d0128ce8c2a606d819297817a8b71a9f05
EddyKaggia <kagzcodes@gmail.com>
2025-06-07 21:31:31 -0700

    feat:Conatiner with most water created and refactored

--- src/main/java/leetcode75/MaxArea.java
+++ src/main/java/leetcode75/MaxArea.java
@@ -1,4 +1,20 @@
 package leetcode75;
 
 public class MaxArea {
+    public int maxArea(int[] height) {
+        int max = 0, left = 0, right = height.length - 1, area;
+
+        while (left < right) {
+            if (height[right] < height[left]) {
+                area = (right - left) * height[right];
+                right--;
+            } else {
+                area = (right - left) * height[left];
+                left--;
+            }
+            max = Math.max(area, max);
+        }
+
+        return max;
+    }
 }
