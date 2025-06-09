49fbfd71c9f9e529148f8cfede014a5a4fdffbd0
EddyKaggia <kagzcodes@gmail.com>
2025-06-08 18:35:25 -0700

    feat:Hashtable hash method added

--- src/main/java/data_structures/hashtable/Main.java
+++ src/main/java/data_structures/hashtable/Main.java
@@ -1,4 +1,9 @@
 package data_structures.hashtable;
 
 public class Main {
+    public static void main (String[] args) {
+        HashTable myTable = new HashTable();
+
+        myTable.printTable();
+    }
 }
